16d83a79a5a19784a81a6746d3c4ad068b47cb95
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2019-11-28 00:40:44 +0100

    wip

--- eslint/babel-eslint-parser/test/fixtures/config/babel.config.js
+++ eslint/babel-eslint-parser/test/fixtures/config/babel.config.js
@@ -2,20 +2,20 @@
 
 module.exports = {
   presets: [
-    ["@babel/preset-env", { forceAllTransforms: true }],
-    ["@babel/preset-flow", { all: true }],
-    "@babel/preset-react",
+    [require.resolve("@babel/preset-env"), { forceAllTransforms: true }],
+    [require.resolve("@babel/preset-flow"), { all: true }],
+    require.resolve("@babel/preset-react"),
   ],
   plugins: [
-    "@babel/plugin-syntax-dynamic-import",
-    "@babel/plugin-syntax-import-meta",
-    "@babel/plugin-syntax-export-default-from",
-    "@babel/plugin-proposal-class-properties",
-    "@babel/plugin-proposal-nullish-coalescing-operator",
-    "@babel/plugin-proposal-optional-chaining",
-    "@babel/plugin-syntax-numeric-separator",
-    "@babel/plugin-syntax-export-namespace-from",
-    ["@babel/plugin-proposal-decorators", { decoratorsBeforeExport: false }],
-    ["@babel/plugin-proposal-pipeline-operator", { proposal: "minimal" }],
+    require.resolve("@babel/plugin-syntax-dynamic-import"),
+    require.resolve("@babel/plugin-syntax-import-meta"),
+    require.resolve("@babel/plugin-syntax-export-default-from"),
+    require.resolve("@babel/plugin-proposal-class-properties"),
+    require.resolve("@babel/plugin-proposal-nullish-coalescing-operator"),
+    require.resolve("@babel/plugin-proposal-optional-chaining"),
+    require.resolve("@babel/plugin-syntax-numeric-separator"),
+    require.resolve("@babel/plugin-syntax-export-namespace-from"),
+    [require.resolve("@babel/plugin-proposal-decorators"), { decoratorsBeforeExport: false }],
+    [require.resolve("@babel/plugin-proposal-pipeline-operator"), { proposal: "minimal" }],
   ],
 };
