250ac649b9c5a320885dc2e367ed0c172acc4d98
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2022-02-13 22:02:29 +0100

    Fix failure

--- packages/babel-plugin-proposal-pipeline-operator/test/fixtures/hack-double-at/class-decorator/output.js
+++ packages/babel-plugin-proposal-pipeline-operator/test/fixtures/hack-double-at/class-decorator/output.js
@@ -1,4 +1,4 @@
-var _initClass, _decorated_class, _ref;
+var _initClass, _decorated_class, _class, _temp, _ref;
 
 const expectedValue = 42;
 
@@ -6,19 +6,15 @@ function decorator(target) {
   target.decoratorValue = expectedValue;
 }
 
-const result = (_ref = expectedValue, (class {
-  static {
-    [_decorated_class, _initClass] = babelHelpers.applyDecs(this, [], [decorator]);
-  }
-
+const result = (_ref = expectedValue, ((_temp = _class = class {
   constructor() {
     this.value = _ref;
   }
 
-  static {
-    _initClass();
-
-  }
-}, _decorated_class));
+}, (() => {
+  [_decorated_class, _initClass] = babelHelpers.applyDecs(_class, [], [decorator]);
+})(), (() => {
+  _initClass();
+})(), _temp), _decorated_class));
 expect(result.decoratorValue).toBe(expectedValue);
 expect(new result().value).toBe(expectedValue);
