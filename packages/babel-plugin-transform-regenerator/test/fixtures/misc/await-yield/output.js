b9f9f8a1442b3061c3eb1ac237e3f07adf031dbc
Nicolò Ribaudo <hello@nicr.dev>
2025-06-03 15:47:39 +0200

    Do not split `awaitAsyncGenerator` in `await yield`

--- packages/babel-plugin-transform-regenerator/test/fixtures/misc/await-yield/output.js
+++ packages/babel-plugin-transform-regenerator/test/fixtures/misc/await-yield/output.js
@@ -1,16 +1,15 @@
 var _marked = /*#__PURE__*/babelHelpers.regenerator().m(f);
 function f() {
-  var res, _t, _t2;
+  var res, _t;
   return babelHelpers.regeneratorAsyncGen(function (_context) {
     while (1) switch (_context.n) {
       case 0:
-        _t = babelHelpers;
         _context.n = 1;
         return 1;
       case 1:
-        _t2 = _context.v;
+        _t = _context.v;
         _context.n = 2;
-        return _t.awaitAsyncGenerator.call(_t, _t2);
+        return babelHelpers.awaitAsyncGenerator(_t);
       case 2:
         res = _context.v;
       case 3:
